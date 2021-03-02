7626b8c8ce2f0249bcfaf19d0901195f4a2571d5
Kartik Gupta <krtk.anurima123@gmail.com>
2021-03-03 00:33:46 +0530

    Added constants

---
+++ constants.py
@@ -0,0 +1,24 @@
+NUMBER_DATA_CENTERS=4
+NUMBER_USERS=4
+COUNT_DATA=3
+NUMBER_REPLICAS=2
+READ_PATTERNS=[[0,1,2,1],
+                [3,1,1,1],
+                [3,1,1,1],
+                [0,0,0,10]]
+WRITE_PATTERNS=[[0,1,2,1],
+                [3,1,1,1],
+                [3,1,1,1],
+                [0,0,0,10]]
+NETWORK_LATENCY=[[0,1,2,1],
+                [3,1,1,1],
+                [3,1,1,1],
+                [0,0,0,10]] # li,j
+AGREED_SLA_COST=[[5,5,5,5],
+                [5,5,5,5],
+                [5,5,5,5],
+                [5,5,5,5]] # Qi,m
+GAMMA=1
+AVG_SERVICE_RATE=[2,2,2,5] # mu
+AVG_ARRIVAL_RATE=[2,2,2,10] # lambda
+NUM_SERVERS_EACH_DATA_CENTER=[3,3,3,3] # Nj 
